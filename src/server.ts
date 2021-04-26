5c49efc9a63441752767f758cd814c9d97938380
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-04-26 19:57:03 -0300

    Formatado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express, { request, response } from 'express'
+import express, { request, response } from "express";
 
-const app = express()
+const app = express();
 
-app.get("/", (request, response)=> {
-    return response.send("ola Mundo")
+app.get("/", (request, response) => {
+  return response.send("ola Mundo");
+});
+
+app.post("/", (request, response)=>{
+    return response.json({message: "Ola usuário"})
 })
 
-app.listen(3333, ()=> console.log("Servidor Rodando na porta 3333"));
+app.listen(3333, () => console.log("Servidor Rodando na porta 3333"));
