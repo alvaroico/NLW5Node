f19a44d40e8aa11a87312314645b8b57d6c5c47d
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-04-26 19:54:09 -0300

    Rota Get

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
+
+app.get("/", (request, response)=> {
+    return response.send("ola Mundo")
+})
 
 app.listen(3333, ()=> console.log("Servidor Rodando na porta 3333"));
