5f9fffdeae5c98dbead56ad10a81a006f3a0b4b1
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-05-04 22:32:09 -0300

    Listando Mensagem

--- src/websocket/clients.ts
+++ src/websocket/clients.ts
@@ -45,5 +45,7 @@ io.on("connect", (socket) => {
       text,
       user_id,
     });
+    const allMessages = await messagesService.listByUser(user_id);
+    socket.emit("client_list_all_messages", allMessages)
   });
 });
