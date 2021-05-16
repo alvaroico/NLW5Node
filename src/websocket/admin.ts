1f78d6ed156d656935fcedd48c39c0886eb4e1f3
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-05-16 15:17:06 -0300

    Recebendo mensagem atendente

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -16,4 +16,18 @@ io.on("connect", async (socket) => {
     const allMessages = await messagesService.listByUser(user_id);
     callback(allMessages);
   });
+  socket.on("admin_send_message", async (params) => {
+    const { user_id, text } = params;
+
+    await messagesService.create({
+      text,
+      user_id,
+      admin_id: socket.id,
+    });
+    const { socket_id } = await connectionsService.findByUserId(user_id);
+    io.to(socket_id).emit("admin_send_to_client", {
+      text,
+      socket_id: socket_id, 
+    });
+  });
 });
